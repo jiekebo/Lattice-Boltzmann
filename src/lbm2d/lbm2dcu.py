c740e10d5ea8d608d38652969d836ee6e24c8aec
Jacob Salomonsen <jiekebo@gmail.com>
2011-05-20 22:45:53 +0200

    Bounceback implemented

--- src/lbm2d/lbm2dcu.py
+++ src/lbm2d/lbm2dcu.py
@@ -13,7 +13,7 @@ from pycuda.compiler import SourceModule
 ' Simulation attributes '
 nx      = 10
 ny      = 10
-it      = 5
+it      = 10
 
 ' Constants '
 omega   = 1.0
@@ -32,32 +32,21 @@ blocksPerGrid   = (nx*ny + threadsPerBlock - 1) / threadsPerBlock
 F           = np.zeros((9,nx,ny), dtype=float).astype(np.float32)
 F[:,:,:]   += density/9.0
 FEQ         = np.copy(F)
+BOUNCEBACK  = np.zeros(F.shape, dtype=float).astype(np.float32)
 DENSITY     = np.zeros((nx,ny), dtype=float).astype(np.float32)
 UX          = np.copy(DENSITY)
 UY          = np.copy(DENSITY)
 
 ' Create the scenery '
-BOUND   = np.zeros((nx,ny), dtype=float)
-BOUNDi  = np.ones(BOUND.shape, dtype=float)
+BBCOUNT = 0
+BOUND   = np.zeros((nx,ny), dtype=float).astype(np.float32)
 for i in xrange(nx):
     for j in xrange(ny):
         if ((i-4)**2+(j-5)**2+(5-6)**2) < 6:
-            BOUND [i,j] = 0.0
-            BOUNDi[i,j] = 0.0
-BOUND [:,0] = 1.0
-BOUNDi[:,0] = 0.0
-
-'''
-msize=nx*ny;
-CI=0:msize:msize*7;
-ON=find(BOUND); %matrix offset of each Occupied Node
-TO_REFLECT=[ON+CI(1) ON+CI(2) ON+CI(3) ON+CI(4) ...
-            ON+CI(5) ON+CI(6) ON+CI(7) ON+CI(8)];
-REFLECTED= [ON+CI(5) ON+CI(6) ON+CI(7) ON+CI(8) ...
-            ON+CI(1) ON+CI(2) ON+CI(3) ON+CI(4)];
-            
-avu=1; prevavu=1; ts=0; deltaU=1e-7; numactivenodes=sum(sum(1-BOUND));
-'''
+            BOUND [i,j] = 1.0
+            BBCOUNT += 1
+#BOUND [:,0] = 1.0
+#BOUNDi[:,0] = 0.0
 
 ' A preliminary kernel treating block index as z-component ' 
 ' x and y field is limited to available threads per block (system dependent) '
@@ -108,52 +97,79 @@ mod = SourceModule("""
         F[7*size + F9] = F[7*size + F8];
     }
     
-    __global__ void bouncebackKernel() {
-        /*BOUNCEDBACK=F(TO_REFLECT);*/
-    }
-    
-    __global__ void densityKernel(float *F, float *D, float *UX, float *UY) {
+    __global__ void densityKernel(float *F, float *BOUND, float * BOUNCEBACK, float *D, float *UX, float *UY) {
         int size = blockDim.x * blockDim.y;
         int cur = threadIdx.x + threadIdx.y * blockDim.x;
-        D[cur] = F[0*size + cur] + 
-                 F[1*size + cur] +
-                 F[2*size + cur] +
-                 F[3*size + cur] +
-                 F[4*size + cur] +
-                 F[5*size + cur] +
-                 F[6*size + cur] +
-                 F[7*size + cur] +
-                 F[8*size + cur];
-        
-        UX[cur] = ((F[0*size + cur] + F[1*size + cur] + F[7*size + cur]) -
-                   (F[3*size + cur] + F[4*size + cur] + F[5*size + cur])) 
-                    / D[cur];
-                 
-        UY[cur] = ((F[1*size + cur] + F[2*size + cur] + F[3*size + cur]) -
-                   (F[5*size + cur] + F[6*size + cur] + F[7*size + cur])) 
-                    / D[cur];
+        if(BOUND[cur] == 1.0f) {
+            BOUNCEBACK[0*size + cur] = F[0*size + cur];
+            BOUNCEBACK[1*size + cur] = F[1*size + cur];
+            BOUNCEBACK[2*size + cur] = F[2*size + cur];
+            BOUNCEBACK[3*size + cur] = F[3*size + cur];
+            BOUNCEBACK[4*size + cur] = F[4*size + cur];
+            BOUNCEBACK[5*size + cur] = F[5*size + cur];
+            BOUNCEBACK[6*size + cur] = F[6*size + cur];
+            BOUNCEBACK[7*size + cur] = F[7*size + cur];
+            D[cur] = 0.0f;
+            UX[cur] = 0.0f;
+            UY[cur] = 0.0f;
+        } else {
+            D[cur] = F[0*size + cur] + 
+                     F[1*size + cur] +
+                     F[2*size + cur] +
+                     F[3*size + cur] +
+                     F[4*size + cur] +
+                     F[5*size + cur] +
+                     F[6*size + cur] +
+                     F[7*size + cur] +
+                     F[8*size + cur];
+            
+            UX[cur] = ((F[0*size + cur] + F[1*size + cur] + F[7*size + cur]) -
+                       (F[3*size + cur] + F[4*size + cur] + F[5*size + cur])) 
+                        / D[cur];
+                     
+            UY[cur] = ((F[1*size + cur] + F[2*size + cur] + F[3*size + cur]) -
+                       (F[5*size + cur] + F[6*size + cur] + F[7*size + cur])) 
+                        / D[cur];
+        }
+    }
+    
+    __global__ void bouncebackKernel(float *F, float *BOUNCEBACK) {
+        int size = blockDim.x * blockDim.y;
+        int cur = threadIdx.x + threadIdx.y * blockDim.x;
+        F[0*size + cur] = BOUNCEBACK[4*size + cur];
+        F[1*size + cur] = BOUNCEBACK[5*size + cur];
+        F[2*size + cur] = BOUNCEBACK[6*size + cur];
+        F[3*size + cur] = BOUNCEBACK[7*size + cur];
+        F[4*size + cur] = BOUNCEBACK[0*size + cur];
+        F[5*size + cur] = BOUNCEBACK[1*size + cur];
+        F[6*size + cur] = BOUNCEBACK[2*size + cur];
+        F[7*size + cur] = BOUNCEBACK[3*size + cur];
     }
     """)
 
+' Allocate memory on the GPU '
+F_gpu       = cuda.mem_alloc(F.size * F.dtype.itemsize)
+BOUND_gpu   = cuda.mem_alloc(BOUND.size * BOUND.dtype.itemsize)
+BOUNCEBACK_gpu = cuda.mem_alloc(BOUNCEBACK.size * BOUNCEBACK.dtype.itemsize)
+DENSITY_gpu = cuda.mem_alloc(DENSITY.size * DENSITY.dtype.itemsize)
+UX_gpu      = cuda.mem_alloc(UX.size * UX.dtype.itemsize)
+UY_gpu      = cuda.mem_alloc(UY.size * UY.dtype.itemsize)
+
 ts=0
 while(ts<it):
-    ' Allocate memory on the GPU '
-    F_gpu       = cuda.mem_alloc(F.size * F.dtype.itemsize)
-    DENSITY_gpu = cuda.mem_alloc(DENSITY.size * DENSITY.dtype.itemsize)
-    UX_gpu      = cuda.mem_alloc(UX.size * UX.dtype.itemsize)
-    UY_gpu      = cuda.mem_alloc(UY.size * UY.dtype.itemsize)
     cuda.memcpy_htod(DENSITY_gpu, DENSITY)
+    cuda.memcpy_htod(BOUND_gpu, BOUND)
+    cuda.memcpy_htod(BOUNCEBACK_gpu, BOUNCEBACK)
     cuda.memcpy_htod(UX_gpu, UX)
     cuda.memcpy_htod(UY_gpu, UY)
     cuda.memcpy_htod(F_gpu, F)
     
-    #F_prop = np.empty_like(F)
-    #cuda.memcpy_dtoh(F, F_gpu)
-    
     prop = mod.get_function("propagateKernel")
-    prop(F_gpu, block=(nx,ny,1))
     density = mod.get_function("densityKernel")
-    density(F_gpu, DENSITY_gpu, UX_gpu, UY_gpu, block=(nx,ny,1))
+    bounceback = mod.get_function("bouncebackKernel")
+    
+    prop(F_gpu, block=(nx,ny,1))
+    density(F_gpu, BOUND_gpu, BOUNCEBACK_gpu, DENSITY_gpu, UX_gpu, UY_gpu, block=(nx,ny,1))
     
     cuda.memcpy_dtoh(F, F_gpu)
     cuda.memcpy_dtoh(DENSITY, DENSITY_gpu)
@@ -161,11 +177,6 @@ while(ts<it):
     cuda.memcpy_dtoh(UY, UY_gpu)
     
     UX[:,0] = UX[:,0]+deltaU
-    '''
-    UX(ON)=0;
-    UY(ON)=0;
-    DENSITY(ON)=0;
-    '''
     U_SQU = pow(UX[:,:],2) + pow(UY[:,:],2)
     U_C2=UX+UY
     U_C4=-UX+UY;
@@ -188,12 +199,8 @@ while(ts<it):
     FEQ[7,:,:]=t3*DENSITY[:,:]*(1+U_C8[:,:]/c_squ+0.5*pow((U_C8[:,:]/c_squ),2)-U_SQU[:,:]/(2*c_squ))
     
     F=omega*FEQ+(1-omega)*F
-    
-    '''
-    F(REFLECTED)=BOUNCEDBACK;
-    prevavu=avu;
-    avu=sum(sum(UX))/numactivenodes; ts=ts+1;
-    '''
+    cuda.memcpy_htod(F_gpu, F)
+    bounceback(F_gpu, BOUNCEBACK_gpu, block=(nx,ny,1))
     ts += 1
 
 
